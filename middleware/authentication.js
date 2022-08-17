72a6d7082a36c5d4e2d48a9d6526a0046b759540
Parth Parmar <pp073522@gmail.com>
2022-08-17 15:55:50 +0530

    added OTP email template

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -24,10 +24,25 @@ function sendOtp(req,res,next) {
 
     //providing options for the mail 
   var mailOptions = {
-    from: 'Fake Store ',
+    from: 'Fake Store',
     to: req.body.email,
-    subject: "OTP",
-    html: `<h3>${otp}</h3>`,
+    subject: "OTP for e-mail verification",
+    html: `
+    <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  <div style="margin:50px auto;width:70%;padding:20px 0">
+    <div style="border-bottom:1px solid #eee">
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Fakestore</a>
+    </div>
+    <p style="font-size:1.1em">Hello,</p>
+    <p>Use the following OTP to complete your process. OTP is valid for 1 minute only</p>
+    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+    <p style="font-size:0.9em;">Regards,<br />Fakestore</p>
+    <hr style="border:none;border-top:1px solid #eee" />
+    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+      <p>Fakestore , India</p>
+    </div>
+  </div>
+</div>`,
   };
 
   //sending the mail to given emails
